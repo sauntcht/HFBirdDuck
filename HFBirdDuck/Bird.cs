beed0abebb03e039fb0b97a2f62e6465588b0a9b
ehtom <ehtom@DESKTOP-SS1Q4KR>
2022-02-23 16:03:43 -0500

    Add project files.

---
+++ HFBirdDuck/Bird.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HFBirdDuck
+{
+    internal class Bird
+    {
+        public string Name { get; set; }
+        public virtual void Fly(string destination)
+        {
+            Console.WriteLine($"{this} is flying to {destination}");
+        }
+        public override string ToString()
+        {
+            return $"A bird named {Name}";
+        }
+        public static void FlyAway(List<Bird> flock, string destination)
+        {
+            foreach (Bird bird in flock)
+            { 
+                bird.Fly(destination);
+            }
+            
+        }
+
+    }
+}
